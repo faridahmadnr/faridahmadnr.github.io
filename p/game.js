70c76e9bfb6f2177f99a1cf00c494871aa4564c5
Nurkhamid <nurkhamid@staff.uny.ac.id>
2021-10-21 00:31:02 +0700

    ada yang baru saya tambahkan

---
+++ p/game.js
@@ -0,0 +1,13 @@
+// JAVASCRIPT CODE//
+
+//select canvas//
+const cvs = document.getElementById("bird");
+const ctx = cvs.getContext("2d");
+
+//game vars and canvas//
+let frames = 0;
+
+const sprite =new
+
+
+ 
